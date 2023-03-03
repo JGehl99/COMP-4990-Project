3dd9310a7aba88a7e64cfe6e157adf7fc660b460
Jarrett Jackson <jrj87@live.ca>
2023-03-02 21:26:36 -0500

    Flask app
    Flask app and video feed functionality in flashapp.py. index.html for basic webpage.

---
+++ flaskapp.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, Response
+import cv2
+
+# setup
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+
+# capture video feed
+def gen_frames():
+    while True:
+        success, frame = camera.read()
+        
+        if not success:
+            break
+        else:
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: immage/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+# render html            
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# render feed
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+#run
+if __name__ == "__main__":
+    app.run(debug=True)
